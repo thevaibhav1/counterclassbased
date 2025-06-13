82a82730268ee9356cf71165793c4969777b5d85
thevaibhav1 <vaibhavsahu593@gmail.com>
2025-06-13 12:05:10 +0530

    'counter'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = { count: 0 };
   }
   increment = () => {
